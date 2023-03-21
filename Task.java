fb88e3522e742deee6c8937b022f99227e598d3e
Philipp Kurpekov <p.kurpekov@gmail.com>
2023-03-21 12:12:55 +0300

    final task test done

--- Task.java
+++ Task.java
@@ -14,8 +14,7 @@ public class Task {
     }
 
     public static void main(String[] args) {
-        String[] inputString = new String[] { "22", ";-)", "hello", "45", "world", "Comission", "string", "0", "1",
-                "test", "ghost", "1223355","","7" };
+        String[] inputString = new String[] {  "world", "Comission", "string" };
         System.out.println("базовый набор строк :" + Arrays.toString(inputString));
         System.out.println("строки длиной <=3 :" +Arrays.toString(findShortString(inputString)));
     }
