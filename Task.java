fe584444222147bd5723f9cd63b38b719cea3e5c
Philipp Kurpekov <p.kurpekov@gmail.com>
2023-03-21 12:14:07 +0300

    file task modified

--- Task.java
+++ Task.java
@@ -14,7 +14,8 @@ public class Task {
     }
 
     public static void main(String[] args) {
-        String[] inputString = new String[] {  "world", "Comission", "string" };
+        String[] inputString = new String[] {"22", ";-)", "hello", "45", "world", "Comission", "string", "0", "1",
+                "test", "ghost", "1223355","","7" };
         System.out.println("базовый набор строк :" + Arrays.toString(inputString));
         System.out.println("строки длиной <=3 :" +Arrays.toString(findShortString(inputString)));
     }
